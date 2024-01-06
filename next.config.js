9db30b1c5f501de6809551403939ae13f4b1e44f
Aryan Pai <87257235+MrStudentGuy@users.noreply.github.com>
2024-01-06 18:40:19 +0000

    Fix p2 i think

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPWA = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
